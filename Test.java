26e30fd2eedd48faa9f918fa1b1434ff5f0fc663
islemnaski <153425221+islemnaski@users.noreply.github.com>
2023-12-28 21:37:04 +0100

    Test.java

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+	public static void main(String[] args) {
+		Secretaire sec = new Secretaire("Sarra","ben ahmed","12345678",61235);
+		Medecin med = new Medecin ("Ahmed","ben aisa","13321467", 96527);
+		
+		sec.créer_fiche_patient(new Fiche_patient(120, new Patient(12345678," Ahmed", "magouri",21, 27369246) new Date(22,11,2023) );
+		sec.gérer_rendez_vous(new Rendez_vous(new Date(13,12,2023) ,21));
+		med.gérer_dossier(new Dossier_medical(123,21);
+	}
+}
+
